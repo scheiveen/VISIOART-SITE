74f3ad0822571e57e4dc5a2807e954ca193bbf98
Erick Gesser Schaeffer <erick@Ericks-MacBook-Air.local>
2026-03-31 11:22:25 -0300

    analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import CustomCursor from './components/CustomCursor';
-import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
-import Marquee from './components/Marquee';
-import AboutSection from './components/AboutSection';
-import ServicesSection from './components/ServicesSection';
-import PortfolioSection from './components/PortfolioSection';
-import ProcessSection from './components/ProcessSection';
-import ContactSection from './components/ContactSection';
-import Footer from './components/Footer';
-import './App.css';
+import React from "react";
+import CustomCursor from "./components/CustomCursor";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import Marquee from "./components/Marquee";
+import AboutSection from "./components/AboutSection";
+import ServicesSection from "./components/ServicesSection";
+import PortfolioSection from "./components/PortfolioSection";
+import ProcessSection from "./components/ProcessSection";
+import ContactSection from "./components/ContactSection";
+import Footer from "./components/Footer";
+import "./App.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
